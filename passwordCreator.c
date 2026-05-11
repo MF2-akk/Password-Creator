fa51a953936329a3b4124e17a71cd5dd9d45d181
Gleb Golub <gleb@debian>
2026-05-11 21:12:23 +0300

    adding generate function

--- passwordCreator.c
+++ passwordCreator.c
@@ -2,34 +2,45 @@
 #include <string.h>
 #include <time.h>
 #include <stdlib.h>
+ 
+#define MAX_PASSWORD_LENGTH 64
 
-int main() {
-    size_t length;
-    char pass[64];
-    const char alphabet[] =
+const char alphabet[] =
     "abcdefghijklmnopqrstuvwxyz"
     "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     "0123456789"
     "!@#$%^&*()-_=+[]{};:,.<>/?";
-    int sizeOfAlphabet = strlen(alphabet);
-    int inputValue = scanf("%zu", &length);
-    if (inputValue != 1) {
-	    printf("Invalide value");
-	    return 1;
-    }
+   
+int generate(size_t length) {
+  char pass[MAX_PASSWORD_LENGTH];
+  
+  short sizeOfAlphabet = strlen(alphabet);
+  if (length >= sizeof pass) {
+    return 1;
+  }
 
-    if (length >= sizeof pass) {
-	    return 1;
-    }
+  srand((unsigned)time(NULL));
 
-    srand((unsigned)time(NULL));
+  for (size_t i = 0; i < length; ++i) {
+    int randomIndex = rand() % (short)sizeOfAlphabet;
+    pass[i] = alphabet[randomIndex];
+  }
+  pass[length] = '\0';
+  puts(pass);
+  return 0;
+}
 
-    for (size_t i = 0; i < length; ++i) {
-	  int randomIndex = rand() % (int)sizeOfAlphabet;
-	  pass[i] = alphabet[randomIndex];
+int main() {
+    char function[10];
+    size_t length;
+    if (scanf("%s %zu", function, &length) == 2) {
+      if (strcmp(function, "generate") == 0) {
+        generate(length);
+      }
+    } else {
+      printf("Invalide value");
+      return 1;
     }
-    pass[length] = '\0';
-    puts(pass);
 
     return 0;
 }
